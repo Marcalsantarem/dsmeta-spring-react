7e2212e36931cbb0dedc40bf9e5fa23e55ac6ba8
Marcal <marcalsantamre@gmail.com>
2022-11-20 23:12:25 -0300

    Envio de sms pelo botão

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,9 +2,21 @@ import icon from '../../assets/img/notification-icon.svg';
 
 import './styles.css';
 
-function NotificationButton() {
+import axios from "axios";
+import { BASE_URL } from "../../utils/request";
+
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id : number) {
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+            .then(response => { console.log("Sucesso")})
+}
+
+function NotificationButton({saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
